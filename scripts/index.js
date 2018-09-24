0dd4e42faf21dfa1a8e1806da537f7cf50c943c0
Nicolas GM <nicolasgomez.andap@gmail.com>
2018-09-24 16:13:06 -0500

    Version 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,46 @@
-console.log("Hola Mundo");
+var num1 = 10.5;
+var num2 = 5;
+
+var res = num1 * num2;
+
+var nom = "Nicolas Gomez Morales";
+
+var prf = 'Desarrollador Full Stack';
+
+var tiene_mascota = true;
+
+var datos=["Nicolas",11,"Gomez",22,"Morales",false];  
+
+console.log(res);
+console.log("Mi nombre es "+nom);
+console.log("Profesion : "+prf);
+console.log(tiene_mascota);
+console.log(datos[0]+" "+datos[1]+" "+datos[2]);
+console.log(datos[3]+" "+datos[4]+" "+datos[5]);
+
+var edad = 41;
+
+if (edad>=18 && edad<=40) {
+	console.log("Eres un adulto joven");
+} else {
+	console.log("No eres un adulto joven");
+}
+
+var dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+
+for (var i = 0; i < 7; i++) {
+	console.log(dias[i]);
+}
+
+for (i in dias) {
+	alert(dias[i]);
+}
+
+function sumas(num1,num2) {
+	var res = num1+num2;
+	return res;
+}
+
+var res = sumas(88,55);
+
+console.log(res);
